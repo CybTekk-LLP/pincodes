554b9582f2c130b8ac59b1726f6e7f45717ac639
Shubhcyb2611 <s.gupta@cybtekk.com>
2023-11-08 12:46:20 +0530

    feat: file sytem

---
+++ main.js
@@ -0,0 +1,51 @@
+let pincodeData = new Object();
+let map = new Map();
+
+const getPinCodeData = async () => {
+  await fetch("./pincodes.json")
+    .then((response) => {
+      return response.json();
+    })
+    .then((data) => {
+      getPinCode(new Object(data));
+    })
+    .catch((err) => {
+      console.log(err);
+    });
+};
+getPinCodeData();
+
+const getPinCode = (pincodeData) => {
+  Object.entries(pincodeData).forEach((item) => {
+    if (!map.has(item[1].pincode)) {
+      map.set(item[1].pincode, { data: item[1] });
+    }
+  });
+  convertToJSON(map);
+};
+
+const convertToJSON = (map) => {
+  let obj = new Object();
+  for (let [key, value] of map) {
+    obj[key] = value;
+  }
+  saveFile(obj);
+};
+
+async function saveFile() {
+  try {
+    // create a new handle
+    const newHandle = await window.showSaveFilePicker();
+
+    // create a FileSystemWritableFileStream to write to
+    const writableStream = await newHandle.createWritable();
+
+    // write our file
+    await writableStream.write(obj);
+
+    // close the file and write the contents to disk.
+    await writableStream.close();
+  } catch (err) {
+    console.error(err.name, err.message);
+  }
+}
